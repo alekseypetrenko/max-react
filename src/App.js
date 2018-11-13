5c437bee2ad47896492b8abf0a66fc2a581bc3c7
OlexiiPetrenko <olexii.petrenko@bazaarvoice.com>
2018-11-13 19:14:56 +0200

    next

--- src/App.js
+++ src/App.js
@@ -29,25 +29,35 @@ class App extends Component {
     })
   }
   render() {
+    const style = {
+      backgroundColor: "white",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer"
+    }
     return (
       <div className="App">
         <h1>React</h1>
-        <button onClick={() => this.switchNameHandler("Alex")}>
+        <button style={style} onClick={this.togglePersonHandler}>
           Switch Name
         </button>
-        <Person 
-          name={this.state.persons[0].name} 
-          age={this.state.persons[0].age}/>
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          clickInApp={this.switchNameHandler.bind(this, "Aleee!!!")}
-          changed={this.nameChangeHandler}
-          >Hobbies: Racing
-        </Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}/>
+        <div>
+          <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age}
+            />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age}
+            clickInApp={this.switchNameHandler.bind(this, "Aleee!!!")}
+            changed={this.nameChangeHandler}
+            >Hobbies: Racing
+          </Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}/>
+        </div>
       </div>
     );
     //return React.createElement("div", {className: "App"}, React.createElement("h1", null, "some"));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
-
+import "./Person.css"
 
 class Person extends Component {
     render() {
         return (
-            <div>
+            <div className="Person">
                 <p onClick={this.props.clickInApp}>I'm {this.props.name} and I am {this.props.age} years old</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name}/>
